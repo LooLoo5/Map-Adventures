12bb527230caa2dc150e6746276fc6279c772e95
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2019-01-23 16:43:40 -0600

    background and navbar

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
-export default class Nav extends React.Component {
+export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
 
