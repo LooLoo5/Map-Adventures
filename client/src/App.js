aaa1bbb1b0d817dffe5c1847bb3c73f674c51161
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2019-01-23 20:38:45 -0600

    jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Nav from "./Nav"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from "./Container";
 import "./App.css"
 
 
@@ -9,7 +8,6 @@ class App extends Component {
   render() {
     return (
 
-      <Container/>,
       <Nav/>
 
     );
--- client/src/Nav.js
+++ client/src/Nav.js
@@ -1,25 +1,14 @@
 import React from 'react';
+import {Container} from 'reactstrap';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Jumbotron, Button } from 'reactstrap';
 
-export default class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
+export default class This extends React.Component {
 
   render() {
     return (
       <div>
+        <Container>
         <Nav tabs>
           <NavItem>
             <NavLink className="text-success" href="#" active>Link</NavLink>
@@ -33,8 +22,28 @@ export default class Navbar extends React.Component {
           <NavItem>
             <NavLink className="text-success" href="#">Link</NavLink>
           </NavItem>
+          <NavItem>
+            <NavLink className="text-success" href="#">Sign Up Here!</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="text-success" href="#">Login Here!</NavLink>
+          </NavItem>
         </Nav>
+        </Container>
+      </div>,
+      <div>
+      <Container>
+        <Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
+        </Container>
       </div>
-    );
+      )
+    }
   }
-}
