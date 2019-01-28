8e8594e4e3b807ab72f558e9b0712c3f8d1417d9
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2019-01-28 17:46:38 -0600

    worked on styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import Nav from "./Nav"
+import Nav from "./Nav";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css"
+import "./App.css";
+import  Jumbotron from "./Jumbotron";
+import Form from "./Form";
 
 
 class App extends Component {
   render() {
     return (
-
+      <div className="App">
       <Nav/>
-
+      <Jumbotron/>
+      <Form/>
+      </div>
     );
   }
 }
